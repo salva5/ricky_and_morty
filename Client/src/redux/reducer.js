f73fd739309de64df4f1fb092374736d912ad55b
salvador <salvador.cahuana3@gmail.com>
2024-03-13 14:52:46 -0300

    Agregado: Creación de characters en el estado global

--- Client/src/redux/reducer.js
+++ Client/src/redux/reducer.js
@@ -1,42 +1,73 @@
-import { ADD_FAV, REMOVE_FAV, ORDER, FILTER } from "./types-actions"
-
+import {
+  ADD_FAV,
+  REMOVE_FAV,
+  ORDER,
+  FILTER,
+  GET_CHARACTERS_ID,
+  REMOVE_CHARACTER,
+  DELETE_CHARACTERS
+} from "./types-actions";
 
 const initialState = {
-    myFavorites: [],
-    allCharacters: []
-}
-const reducer = (state = initialState, {type, payload}) => {
-    switch(type) {
-        case ADD_FAV:
-            return {
-                ...state,
-                myFavorites: payload,
-                allCharacters: payload
-            };
-        case REMOVE_FAV:
-            return {
-                ...state,
-                myFavorites: payload,
-                allCharacters:payload
-            };
-        case FILTER:
-            const filtercharacters = state.allCharacters.filter(char => char.gender === payload)
-            return {
-                ...state,
-                myFavorites: payload === "Todos" ? [...state.allCharacters]: filtercharacters
-            }
-        case ORDER:
-            const anotherStateCopy = [...state.myFavorites]
-            const orderFav = payload === "A" ? anotherStateCopy.sort((a,b) => a.id - b.id)
-            : anotherStateCopy.sort((a,b) => b.id - a.id)
-            return {
-                ...state,
-                myFavorites: orderFav
-            }   
-        default:
-            return{...state};
+  characters: [],
+  myFavorites: [],
+  copyMyFavorites: [],
+};
+const reducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case GET_CHARACTERS_ID:
+      return {
+        ...state,
+        characters: [...state.characters, payload]
+      }
+    case REMOVE_CHARACTER :{
+      const newCharacters = state.characters.filter(
+        (character) => character.id !== payload
+      );
+      return {
+        ...state,
+        characters: newCharacters
+      }
     }
-}
-
+    case DELETE_CHARACTERS:
+      return {
+        ...state,
+        characters: []
+      }
+    case ADD_FAV:
+      return {
+        ...state,
+        myFavorites: payload,
+        copyMyFavorites: payload,
+      };
+    case REMOVE_FAV:
+      return {
+        ...state,
+        myFavorites: payload,
+        copyMyFavorites: payload,
+      };
+    case FILTER:
+      const filtercharacters = state.copyMyFavorites.filter(
+        (char) => char.gender === payload
+      );
+      return {
+        ...state,
+        myFavorites:
+          payload === "Todos" ? [...state.copyMyFavorites] : filtercharacters,
+      };
+    case ORDER:
+      const anotherStateCopy = [...state.myFavorites];
+      const orderFav =
+        payload === "A"
+          ? anotherStateCopy.sort((a, b) => a.id - b.id)
+          : anotherStateCopy.sort((a, b) => b.id - a.id);
+      return {
+        ...state,
+        myFavorites: orderFav,
+      };
+    default:
+      return { ...state };
+  }
+};
 
 export default reducer;
