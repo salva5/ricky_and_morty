97b821b55a2aa83c420f054def33b78e4254584f
salvador <salvador.cahuana3@gmail.com>
2024-03-23 17:09:07 -0300

    refactor:  uso de hook creado para filtrar en lugar de hacer dispatch

--- Client/src/components/Favorites/Favorites.jsx
+++ Client/src/components/Favorites/Favorites.jsx
@@ -1,58 +1,57 @@
-import { connect } from "react-redux";
 import Card from "../Card/Card";
-import { useDispatch } from "react-redux";
 import { useState } from "react";
 import { filterCards, orderCards } from "../../redux/actions/actions";
-const Favorites = ({myFavorites}) => {
-    const [aux, setAux] = useState(false)
-    const dispatch = useDispatch()
+import { useFavoritesFilters } from "../../hooks/useFavoritesFilters";
+import { useDispatch } from "react-redux";
+const Favorites = () => {
+  const dispatch = useDispatch()
+  const { favorites, setGender } = useFavoritesFilters()
+  const [aux, setAux] = useState(false);
 
-    const handleOrder = (event) => {
-        dispatch(orderCards(event.target.value))
-        setAux(true)
-    }
-    const handleFilter = (event) => {
-        dispatch(filterCards(event.target.value))
-    }
-    return (
-        <div>
+  const handleOrder = (event) => {
+    dispatch(orderCards(event.target.value));
+    setAux(true);
+  };
 
-            <select onChange={handleOrder}>
-                <option value="A">Ascendente</option>
-                <option value="D">Descendente</option>
-            </select>
-            <select onChange={handleFilter}>
-                <option value="Todos">Todos</option>
-                <option value="Male">Male</option>
-                <option value="Female">Female</option>
-                <option value="Genderless">Genderless</option>
-                <option value="unknown">unknown</option>
-            </select>
-            {
-                myFavorites.map(({id, name, status, species, gender, origin, image,onClose}) => { 
-                    return(
-                        <Card
-                            key ={id}
-                            id= {id}
-                            name={name}
-                            status={status}
-                            species={species}
-                            gender={gender}
-                            origin={origin}
-                            image={image}
-                            onClose={onClose}
-                        />
+  const handleFilter = (event) => {
+    setGender(event.target.value)
+  };
 
-                    )
-                })
-            }
-        </div>
-    )
-}
-const mapStateToProps = (state) => {
-    return {
-        myFavorites: state.myFavorites
-    }
-}
+  
 
-export default connect(mapStateToProps,null)(Favorites);
+  return (
+    <div>
+      <select onChange={handleOrder}>
+        <option value="A">Ascendente</option>
+        <option value="D">Descendente</option>
+      </select>
+      <select onChange={handleFilter}>
+        <option value="Todos">Todos</option>
+        <option value="Male">Male</option>
+        <option value="Female">Female</option>
+        <option value="Genderless">Genderless</option>
+        <option value="unknown">unknown</option>
+      </select>
+      {
+        favorites.map(
+          ({ id, name, status, species, gender, origin, image, onClose }) => {
+            return (
+              <Card
+                key={id}
+                id={id}
+                name={name}
+                status={status}
+                species={species}
+                gender={gender}
+                origin={origin}
+                image={image}
+                onClose={onClose}
+              />
+            );
+          }
+        )
+      }
+    </div>
+  );
+};
+export default Favorites;
